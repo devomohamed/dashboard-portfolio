f03f5d0c6a33fe978ec311314f446fec290c9e3e
Mohamed Sabry <76483404+devomohamed@users.noreply.github.com>
2023-03-14 18:49:51 +0200

    Update video

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,18 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   email!: string;
   password!: string;
 
+  @ViewChild('video', { static: true }) videoElement!: ElementRef;
   onSubmit() {
     // Login logic here
     console.log({email:this.email,password:this.password});
 
+  }
+
+  ngOnInit() {
+    this.videoElement.nativeElement.play();
   }
 
 }
